6ecdeb771a5696b7e355a424f451fceb9715c836
preranakafle <142286052+preranakafle@users.noreply.github.com>
2024-02-08 20:30:28 +0545

    Add files via upload
    home page

---
+++ home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Management System</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">
+                <h2>ByTeBooKs </h2>
+</div>
+<div class="navmenu">
+    <ul>
+        <li> <a href="#home">Home </a> </li>
+        <li> <a href="#categories">Categories </a> </li>
+        <li> <a href="#about"> About </a> </li>
+        <li> <a href="#contactus"> Contact Us </a> </li>
+</ul>
+
+<div class="search-box">
+    <input type="search" class="search" placeholder="Search here.." >
+    <i clas ="bx bx-search"> </i>
+</div>
+</div>
+</nav>
+</header>
+
+<div class="main-header">
+    <div cladd="header">
+        <h5> ByteBooKs </h5>
+        <h2> For All Your Reading Needs </h2>
+        <p> Hello Jyoti, I am prerana kafle, recently studying BCA in 4th semester. So,
+            I'm studying BCA but not interested in coding heheheh. </p>
+
+        <a href="" class="btn"> Read More </a>
+</div>
+
+</header>
+</body>
+</html>
