416a1d5a0a84017356488daf31703bd5b1dff2bb
JyotiNayak9 <nayakjyoti789@gmail.com>
2024-03-23 22:08:52 +0545

    bok details

--- user/bookdetails.php
+++ user/bookdetails.php
@@ -73,92 +73,57 @@ if (is_user_login()) {
                         </div>
                     </div>
                     <div class="span9">
-                        <form class="form-horizontal row-fluid" action="book.php" method="post">
-                            <div class="control-group">
-                                <label class="control-label" for="Search"><b>Search:</b></label>
-                                <div class="controls">
-                                    <input type="text" id="title" name="title" placeholder="Enter Name/ID of Book"
-                                        class="span8" required>
-                                    <button type="submit" name="submit" class="btn">Search</button>
-                                </div>
+                        <div class="content">
+
+                        <div class="module">
+                            <div class="module-head">
+                                <h3>Book Details</h3>
                             </div>
-                        </form>
-                        <br>
+                            <div class="module-body">
                         <?php
-                        if (isset ($_POST['submit'])) {
-                            $s = $_POST['title'];
-                            $sql = "select * from lms.book where BookId='$s' or Title like '%$s%'";
-                        } else
-                            $sql = "select * from lms.book order by Availability DESC";
+                            $x=$_GET['id'];
+                            $sql="select * from LMS.book where BookId='$x'";
+                            $result=$conn->query($sql);
+                            $row=$result->fetch_assoc();    
+                            
+                                $bookid=$row['BookId'];
+                                $name=$row['Title'];
+                                $publisher=$row['Publisher'];
+                                $year=$row['Year'];
+                                $avail=$row['Availability'];
+                                echo "<b>Book ID:</b> ".$bookid."<br><br>";
+                                echo "<b>Title:</b> ".$name."<br><br>";
+                                $sql1="select * from LMS.author where BookId='$bookid'";
+                                $result=$conn->query($sql1);
+                                
+                                echo "<b>Author:</b> ";
+                                while($row1=$result->fetch_assoc())
+                                {
+                                    echo $row1['Author']."&nbsp;";
+                                }
+                                echo "<br><br>";
+                                echo "<b>Publisher:</b> ".$publisher."<br><br>";
+                                echo "<b>Year:</b> ".$year."<br><br>";
+                                echo "<b>Availability:</b> ".$avail."<br><br>";
 
-                        $result = $conn->query($sql);
-                        $rowcount = mysqli_num_rows($result);
-
-                        if (!($rowcount))
-                            echo "<br><center><h2><b><i>No Results</i></b></h2></center>";
-                        else {
-
-
+                                
+                        
+                           
                             ?>
-                            <table class="table" id="tables">
-                                <!-- <thead color:red> -->
-                                    <tr>
-                                        <th>Book id</th>
-                                        <th>Book name</th>
-                                        <th>Availability</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
+                            
+                        <a href="book.php" class="btn btn-primary">Go Back</a>                             
+                               </div>
+                           </div>
+                            </div>
 
-                                    //$result=$conn->query($sql);
-                                    while ($row = $result->fetch_assoc()) {
-                                        $bookid = $row['BookId'];
-                                        $name = $row['Title'];
-                                        $avail = $row['Availability'];
-                                        ?>
-                                        <tr>
-                                            <td>
-                                                <?php echo $bookid ?>
-                                            </td>
-                                            <td>
-                                                <?php echo $name ?>
-                                            </td>
-                                            <td><b>
-                                                    <?php
-                                                    if ($avail > 0)
-                                                        echo "<font color=\"green\">AVAILABLE</font>";
-                                                    else
-                                                        echo "<font color=\"red\">NOT AVAILABLE</font>";
+                </div>
 
-                                                    ?>
-
-                                                </b></td>
-                                            <td>
-                                                <center><a href="bookdetails.php?id=<?php echo $bookid; ?>"
-                                                        class="btn btn-primary">Details</a>
-                                                    <?php
-                                                    if ($avail > 0)
-                                                        echo "<a href=\"issue_request.php?id=" . $bookid . "\" class=\"btn btn-success\">Issue</a>";
-                                                    ?>
-                                                </center>
-                                            </td>
-                                        </tr>
-                                    <?php }
-                        } ?>
-                            </tbody>
-                        </table>
-                    </div>
-                    <!--/.span3-->
-                    <!--/.span9-->
-
-                    <!--/.span3-->
-                    <!--/.span9-->
+                </div>
+                
                 </div>
                 <!--/.span9-->
             </div>
-        </div>
+
         <!--/.container-->
         <div class="footer">
             <div class="container">
